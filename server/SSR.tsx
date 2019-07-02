c2c5b6e32556f77b089c1d856233f18ecc2d6a34
Ludovit Kapusta <ludovit.kapusta@gmail.com>
2019-07-02 16:12:06 +0200

    adding render method for accepting a data

--- server/SSR.tsx
+++ server/SSR.tsx
@@ -4,12 +4,17 @@ import ReactDOMServer from 'react-dom/server'
 
 import Heading from '../src/Heading'
 
+/* taking care of render component and providing data from React.NET */
+const render = (Element: any, { general = {}, config = {} }: any) => (
+  <Element data={config} general={general} />
+)
+
 declare let global: any
 
 global.React = React
 global.ReactDOM = ReactDOM
 global.ReactDOMServer = ReactDOMServer
 
-global.Heading = Heading
+global.Heading = (initialProps: any) => render(Heading, initialProps)
 
 export default global
